2f979e756e210cc954f3b2f2a6ae982bb402219e
gmgb <gabriellegayno@gmail.com>
2024-05-26 18:19:45 -0300

    new file index

---
+++ index.js
@@ -0,0 +1,31 @@
+import entradaDados from 'readline-sync';
+console.log('Aplicação de juros:\n');
+
+let divida = entradaDados.question('Informe o valor devido: R$ ');
+let dias = entradaDados.question('Informe quantos dias se passaram desde o vencimento do boleto: ');
+
+let juros1 = divida * 0.10;
+let juros2 = divida * 0.05;
+
+let juros_aplicado = (dias >= 15) && juros1;
+
+console.log("\nValor original da dívida: "+(divida));
+console.log("Dias atrasados: "+dias);
+
+let total1 = Number(divida) + Number(juros1);
+let total2 = Number(divida) + Number(juros2);
+
+if (juros_aplicado == juros1){
+    console.log("Taxa de Juros: 10%");
+    console.log("Total da divida: " +total1)
+}
+else{
+    console.log("Taxa de Juros: 5%");
+    console.log("Total da dívida: " +total2)
+};
+
+
+
+
+
+
